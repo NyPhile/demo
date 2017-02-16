76ad56b9378933528750306dcc0adddddec1c289
wang_zhen <wang_zhen@corp.netease.com>
2017-02-16 14:07:11 +0800

    add v1

---
+++ demo.js
@@ -0,0 +1,35 @@
+(function(){
+	"use strict";
+	function preventDefault(ev) {
+		ev.preventDefault();
+	}
+	document.addEventListener('touchmove', preventDefault, false);
+
+	var checkbox = NE('#checkbox'),
+		oCheckwrap = NE('#checkwrap'),
+		loading = document.getElementById('loading'),
+		tip = document.getElementById('tip'),
+		check = document.getElementById('check')
+		;
+	var isCheck = false;
+
+	checkbox.bind("click",function(){
+		if(!isCheck){
+			oCheckwrap.hide();
+			loading.style.display = "block";
+			setTimeout(function(){
+				loading.style.display = "none";
+				check.style.display = "block";
+				tip.innerHTML = "I am not a robot!";
+			},3000)
+			isCheck = true;
+		}
+	});
+	NE(".submit").bind("click",function(){
+		if(isCheck){
+			alert("提交成功")
+		}else{
+			alert("请点击验证")
+		}
+	})
+})();
